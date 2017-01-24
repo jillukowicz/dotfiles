740253ab7a204697ff15ed80d77623c8f197fb14
jillukowicz <jakub.illukowicz@gmail.com>
2017-01-24 09:47:23 +0100

    intstalled omf

--- config.fish
+++ config.fish
@@ -1,3 +1,10 @@
+# Path to Oh My Fish install.
+set -q XDG_DATA_HOME
+  and set -gx OMF_PATH "$XDG_DATA_HOME/omf"
+  or set -gx OMF_PATH "$HOME/.local/share/omf"
+
+# Load Oh My Fish configuration.
+source $OMF_PATH/init.fish
 # powerline-daemon -q
 # i
 # set fish_function_path $fish_function_path "/usr/local/lib/python2.7/site-packages/powerline/bindings/fish"
